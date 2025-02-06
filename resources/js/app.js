1110cc7b8cfc6116704e069b18794439079d8b08
Fajar-FitAcademy <fajar.fitacademy@gmail.com>
2025-02-06 20:54:49 +0700

    chore: migrate tailwind css to v4

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,65 @@ import "../css/app.css";
 
 import.meta.glob(["../images/**", "../fonts/**"]);
 import Alpine from "alpinejs";
+import { Carousel } from "flowbite";
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const cremocarousel = document.getElementById("cremo-carousel");
+
+const items = [
+    {
+        position: 0,
+        el: document.getElementById("cremo-carousel-item-1"),
+    },
+    {
+        position: 1,
+        el: document.getElementById("cremo-carousel-item-2"),
+    },
+    {
+        position: 2,
+        el: document.getElementById("cremo-carousel-item-3"),
+    },
+];
+
+const indicators = [
+    {
+        position: 0,
+        el: document.getElementById("cremo-carousel-indicator-1"),
+    },
+    {
+        position: 1,
+        el: document.getElementById("cremo-carousel-indicator-2"),
+    },
+    {
+        position: 2,
+        el: document.getElementById("cremo-carousel-indicator-3"),
+    },
+];
+
+const options = {
+    defaultPosition: 0,
+    interval: 3000,
+
+    indicators: {
+        activeClasses: "bg-green-300",
+        inactiveClasses: "bg-white",
+        items: indicators,
+    },
+};
+
+// instance options object
+const instanceOptions = {
+    id: "cremo-carousel",
+    override: true,
+};
+
+const carousel = new Carousel(cremocarousel, items, options, instanceOptions);
+
+const carouselPrevButton = document.getElementById("cremo-carousel-prev");
+const carouselNextButton = document.getElementById("cremo-carousel-next");
+
+carouselPrevButton.addEventListener("click", () => carousel.prev());
+carouselNextButton.addEventListener("click", () => carousel.next());
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,7 +23,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-hidden">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
@@ -59,7 +59,7 @@
             <!-- Hamburger -->
             <div class="flex items-center -me-2 sm:hidden">
                 <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 focus:text-gray-500">
                     <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
