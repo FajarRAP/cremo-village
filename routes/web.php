130ddf9afd7e6cdc5e030ffa0ff8f70f398d6f38
Fajar-FitAcademy <fajar.fitacademy@gmail.com>
2025-02-08 21:00:30 +0700

    refactor (web): change how to process home route to arrow function

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', fn() => view('home'));
 
 Route::get('/dashboard', function () {
     return view('dashboard');
