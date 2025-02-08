7828d2386d9e0235b769b0c71083da1044ebdecb
Fajar-FitAcademy <fajar.fitacademy@gmail.com>
2025-02-08 21:01:18 +0700

    feat: add section heading component

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,6 @@
+@props(['text'])
+
+<h4
+    class="text-3xl text-center font-semibold after:ml-0.5 relative after:text-red-500 after:content-[''] after:absolute after:start-1/2 after:-translate-x-1/2 after:border after:border-black after:w-32 after:-bottom-5">
+    {{ $text }}
+</h4>
