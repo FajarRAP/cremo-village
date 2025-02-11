0241f6ef8a19a0b02592b237ada20e2e02d80750
Fajar-FitAcademy <fajar.fitacademy@gmail.com>
2025-02-12 00:07:08 +0700

    refactor (person card): redesign card

--- resources/views/components/person-card.blade.php
+++ resources/views/components/person-card.blade.php
@@ -1,6 +1,6 @@
 @props(['name', 'position', 'asset'])
 
-<figure class="border border-gray-300 rounded-sm">
+<figure class="rounded-sm shadow-md">
     <img src="{{ Vite::asset("resources/images/stakeholders/$asset") }}" alt="stakeholder" class="rounded-t-sm">
     <figcaption class="space-y-1 py-2 flex flex-col items-center">
         <h5 class="font-medium text-xl">{{ $position }}</h5>
