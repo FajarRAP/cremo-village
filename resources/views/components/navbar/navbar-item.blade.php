88414ae383cb092fece75cf2d23392af8e977c9b
FajarRiansyah <fajary781@gmail.com>
2025-02-13 22:52:29 +0700

    refactor (navbar item): remove focus state

--- resources/views/components/navbar/navbar-item.blade.php
+++ resources/views/components/navbar/navbar-item.blade.php
@@ -1,6 +1,6 @@
 @props(['text'])
 
 <a
-    {{ $attributes->merge(['class' => 'block py-2 px-3 rounded-sm md:hover:text-green-400 md:p-0 focus:text-white focus:bg-green-400 text-white transition duration-150']) }}>
+    {{ $attributes->merge(['class' => 'block py-2 px-3 rounded-sm md:hover:text-green-400 md:p-0 text-white transition duration-150']) }}>
     {{ $text }}
 </a>
