009ccf2091d3e406540be2ce09c0d2cf86ae208f
FajarRiansyah <fajary781@gmail.com>
2025-02-15 00:43:46 +0700

    refactor (app): adjust app to be used for dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,18 +18,6 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
-
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white shadow-sm">
-                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
-
-        <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
