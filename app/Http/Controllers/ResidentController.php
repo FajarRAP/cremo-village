10e23567a3635cdabe349b34dd16a19cb7ac1e5f
FajarRiansyah <fajary781@gmail.com>
2025-02-18 01:07:43 +0700

    feat: add resident controller and index. store, edit, and update methods

---
+++ app/Http/Controllers/ResidentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resident;
+use Illuminate\Http\Request;
+
+class ResidentController extends Controller
+{
+
+    public function index()
+    {
+        return view('dashboard.resident-records', [
+            'residents' => Resident::paginate(10),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'rt' => 'required',
+            'rw' => 'required',
+            'birth_date' => 'required',
+        ]);
+
+        Resident::create($validated);
+
+        return redirect(route('dashboard.resident-records', absolute: false));
+    }
+
+    public function edit(Resident $resident)
+    {
+        return view('dashboard.resident-records.edit', [
+            'resident' => $resident,
+        ]);
+    }
+
+    public function update(Request $request, Resident $resident)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'rt' => 'required',
+            'rw' => 'required',
+            'birth_date' => 'required',
+        ]);
+
+        $resident->update($validated);
+
+        return redirect(route('dashboard.resident-records', absolute: false));
+    }
+}
