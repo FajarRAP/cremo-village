ffdf04cb8b055e80c0543e9e79af398ea3f2603b
FajarRiansyah <fajary781@gmail.com>
2025-02-18 22:01:46 +0700

    feat: restructure dashboard and resident records route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResidentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('home'))->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function () {
+    Route::controller(ResidentController::class)->group(function () {
+        Route::get('/resident-records', 'index')->name('dashboard.resident-records');
+        Route::get('/resident-records/{resident}/edit', 'edit')->name('resident.edit');
+        Route::post('/resident-records', 'store')->name('resident.store');
+        Route::put('/resident-records/{resident}', 'update')->name('resident.update');
+        Route::delete('/resident-records/{resident}', 'delete')->name('resident.delete');
+    });
+
+
+
+    Route::get('/', fn() => view('dashboard.dashboard'))->name('dashboard');
+    Route::get('/news', fn() => view('dashboard.news'))->name('dashboard.news');
+    Route::get('/agendas', fn() => view('dashboard.agendas'))->name('dashboard.agendas');
+    Route::get('/guest-book', fn() => view('dashboard.guest-book'))->name('dashboard.guest-book');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
