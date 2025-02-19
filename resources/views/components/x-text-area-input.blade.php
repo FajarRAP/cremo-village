d47722dec68e6623dda4a3daf9a56dfa43e75921
FajarRiansyah <fajary781@gmail.com>
2025-02-19 21:12:38 +0700

    feat: add text area input component

---
+++ resources/views/components/x-text-area-input.blade.php
@@ -0,0 +1,4 @@
+@props(['disabled' => false, 'value' => null])
+
+<textarea @disabled($disabled)
+    {{ $attributes->merge(['class' => 'border-gray-300 focus:border-green-500 focus:ring-green-500 rounded-md shadow-xs']) }}>{{ $value }}</textarea>
