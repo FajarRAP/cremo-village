bdcc3ed05ca92c8ad400b9fe2c7caabe899dccfa
FajarRiansyah <fajary781@gmail.com>
2025-02-19 21:13:49 +0700

    feat: enhance resident records search functionality and improve modal interactions

--- resources/views/dashboard/resident-records/index.blade.php
+++ resources/views/dashboard/resident-records/index.blade.php
@@ -1,19 +1,17 @@
 <x-dashboard-layout>
-    <section class="h-full md:ml-64 pt-20 pb-6 bg-gray-100">
+    <section class="h-full md:ml-64 pt-20 pb-6 bg-gray-100" x-data="{ resident: null }">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
             <div class="bg-white relative shadow-md rounded-lg overflow-hidden">
                 <div
                     class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                     <div class="w-full md:w-1/2">
-                        <form class="flex items-center">
-                            <label for="simple-search" class="sr-only">Search</label>
+                        <form action="{{ route('dashboard.resident-records') }}" method="GET" class="flex items-center">
                             <div class="relative w-full">
                                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <x-svgs.magnifying-glass class="fill-gray-500" />
                                 </div>
-                                <input type="text" id="simple-search"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full pl-10 p-2"
-                                    placeholder="Search" required="">
+                                <x-text-input id="simple-search" class="pl-10 p-2 w-full"
+                                    placeholder="{{ __('Search') }}" name="keyword" />
                             </div>
                         </form>
                     </div>
@@ -68,7 +66,8 @@
                                                 <li>
                                                     <button type="button" data-modal-target="readProductModal"
                                                         data-modal-toggle="readProductModal"
-                                                        class="flex w-full items-center py-2 px-4 hover:bg-gray-100 ">
+                                                        class="flex w-full items-center py-2 px-4 hover:bg-gray-100"
+                                                        x-on:click="resident = {{ json_encode($resident) }}">
                                                         <x-svgs.eye class="mr-2 fill-gray-500" />
                                                         {{ __('Preview') }}
                                                     </button>
@@ -87,13 +86,10 @@
             </div>
         </div>
 
-        <!-- Create modal -->
         <div id="createProductModal"
             class="hidden overflow-y-auto bg-gray-900/40 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full">
             <div class="relative p-4 w-full max-w-2xl max-h-full">
-                <!-- Modal content -->
                 <div class="relative p-4 bg-white rounded-lg shadow sm:p-5">
-                    <!-- Modal header -->
                     <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
                         <h3 class="text-lg font-semibold text-gray-900 Product">
                             {{ __('Add') . ' ' . __('Resident Records') }}</h3>
@@ -103,7 +99,6 @@
                             <x-svgs.xmark />
                         </button>
                     </div>
-                    <!-- Modal body -->
                     <form action="{{ route('resident.store') }}" method="POST">
                         @csrf
                         <div class="grid gap-4 mb-4 sm:grid-cols-2">
@@ -134,13 +129,10 @@
             </div>
         </div>
 
-        <!-- Read modal -->
         <div id="readProductModal"
             class="hidden overflow-y-auto bg-gray-900/40 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full">
             <div class="relative p-4 w-full max-w-xl max-h-full">
-                <!-- Modal content -->
                 <div class="relative p-4 bg-white rounded-lg shadow sm:p-5">
-                    <!-- Modal header -->
                     <div class="flex justify-between mb-4 rounded-t sm:mb-5">
                         <div class="text-lg text-gray-900 md:text-xl ">
                             <h3 class="font-semibold ">Apple iMac 27”</h3>
@@ -162,47 +154,15 @@
                     </div>
                     <dl>
                         <dt class="mb-2 font-semibold leading-none text-gray-900 ">Details</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5 ,3.8GHz
+                        <dd class="mb-4 font-light text-gray-500 sm:mb-5">3.8GHz
                             8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4
                             memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet,
                             Magic Mouse 2, Magic Keyboard - US.</dd>
-                        <dt class="mb-2
-                            font-semibold leading-none text-gray-900 ">Category</dt>
+                        <dt class="mb-2 font-semibold leading-none text-gray-900">Category</dt>
                         <dd class="mb-4 font-light text-gray-500 sm:mb-5 ">Electronics/PC</dd>
                     </dl>
-                    <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-3 sm:space-x-4">
-                            <button type="button"
-                                class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-                                <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor"
-                                    viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d=" M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0
-                            000-2.828z" />
-                        <path fill-rule="evenodd"
-                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                            clip-rule="evenodd" />
-                        </svg>
-                        Edit
-                        </button>
-                        <button type="button"
-                            class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-green-700 focus:z-10 focus:ring-4 focus:ring-gray-200 ">Preview</button>
                 </div>
-                <button type="button"
-                    class="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-                    <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewbox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    Delete
-                </button>
             </div>
-        </div>
-        </div>
-        </div>
-
         </div>
     </section>
 </x-dashboard-layout>
