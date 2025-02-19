3f428d47c8a976a40c9fab985a67a7c413cd6e79
FajarRiansyah <fajary781@gmail.com>
2025-02-19 21:16:22 +0700

    feat: add routes for GuestBookController with CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GuestBookController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResidentController;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => view('home'))->name('home');
 
 Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', fn() => view('dashboard.dashboard'))->name('dashboard');
+
     Route::controller(ResidentController::class)->group(function () {
         Route::get('/resident-records', 'index')->name('dashboard.resident-records');
         Route::get('/resident-records/{resident}/edit', 'edit')->name('resident.edit');
@@ -15,12 +18,16 @@ Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function ()
         Route::delete('/resident-records/{resident}', 'delete')->name('resident.delete');
     });
 
+    Route::controller(GuestBookController::class)->group(function () {
+        Route::get('/guest-book', 'index')->name('dashboard.guest-book');
+        Route::get('/guest-book/{guestbook}/edit', 'edit')->name('guest-book.edit');
+        Route::post('/guest-book', 'store')->name('resident.store');
+        Route::put('/guest-book/{guestbook}', 'update')->name('guest-book.update');
+        Route::delete('/guest-book/{guestbook}', 'delete')->name('guest-book.delete');
+    });
 
-
-    Route::get('/', fn() => view('dashboard.dashboard'))->name('dashboard');
     Route::get('/news', fn() => view('dashboard.news'))->name('dashboard.news');
     Route::get('/agendas', fn() => view('dashboard.agendas'))->name('dashboard.agendas');
-    Route::get('/guest-book', fn() => view('dashboard.guest-book'))->name('dashboard.guest-book');
 });
 
 Route::middleware('auth')->group(function () {
