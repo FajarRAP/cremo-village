59080fad80c2814dc3b830a001aaaf6b6c222a7f
FajarRiansyah <fajary781@gmail.com>
2025-02-23 13:07:25 +0700

    feat (dashboard): add nearest agenda to dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,11 +1,49 @@
 <x-dashboard-layout>
     <section class="p-4 md:ml-64 h-auto pt-20 space-y-4">
+        {{-- Dashboard Cards --}}
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             <x-dashboard-card :title="$residentCount" description="Penduduk" class="h-44" />
             <x-dashboard-card :title="$guestbookCount" description="Tamu" class="h-44" />
-            <x-dashboard-card title="191102" description="Berita" class="h-44" />
+            <x-dashboard-card title="-" description="Berita" class="h-44" />
         </div>
 
+        {{-- Agenda --}}
+        <div class="shadow-md rounded-lg bg-white p-6 flex flex-col gap-6 border border-gray-200">
+            <h5 class="font-bold text-2xl flex items-center gap-2">
+                {{ __('Agenda') }}
+            </h5>
+
+            <form action="{{ route('dashboard') }}" method="GET" class="max-w-sm flex gap-4">
+                @csrf
+                @method('GET')
+                <input type="month"
+                    class="border-gray-300 focus:border-green-500 focus:ring-green-500 rounded-md shadow-sm w-full p-2 text-lg"
+                    name="date" />
+                <x-primary-button type="submit">Ok</x-primary-button>
+            </form>
+
+            <div class="flex flex-col gap-4">
+                @foreach ($agendas as $agenda)
+                    <div class="p-4 rounded-lg bg-gray-50 border border-gray-200 hover:shadow-lg transition">
+                        <h6 class="font-semibold text-lg text-gray-800">{{ $agenda->title }}</h6>
+                        <div class="flex md:flex-row gap-2 flex-col md:items-center text-gray-600 mt-2">
+                            <div class="flex items-center gap-2 md:basis-3xs">
+                                <x-svgs.calendar-days class="size-5 text-green-600" />
+                                <span>{{ $agenda->date }}</span>
+                            </div>
+                            <div class="flex items-center gap-2">
+                                <x-svgs.location-dot class="size-5 text-green-600" />
+                                <span>{{ $agenda->location }}</span>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                <a href="{{ route('dashboard.agendas') }}"
+                    class="font-medium text-green-600 hover:underline self-end">{{ __('Read more') }}</a>
+            </div>
+        </div>
+
+        {{-- Resident Records --}}
         <div class="shadow-md rounded bg-white p-4 flex flex-col gap-4">
             <h5 class="font-semibold text-2xl">{{ __('Resident Records') }}</h5>
             <div class="overflow-x-auto shadow-sm rounded">
@@ -38,6 +76,7 @@
                 class="font-medium text-green-600 hover:underline self-end">{{ __('Read more') }}</a>
         </div>
 
+        {{-- Guest Book --}}
         <div class="shadow-md rounded bg-white p-4 flex flex-col gap-4">
             <h5 class="font-semibold text-2xl">{{ __('Guest Book') }}</h5>
             <div class="overflow-x-auto shadow-sm rounded">
@@ -71,5 +110,6 @@
             <a href="{{ route('dashboard.guest-book') }}"
                 class="font-medium text-green-600 hover:underline self-end">{{ __('Read more') }}</a>
         </div>
+
     </section>
 </x-dashboard-layout>
