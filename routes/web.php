8b1b0059e020997e544c8468e9320520085f2c98
FajarRiansyah <fajary781@gmail.com>
2025-02-24 00:09:00 +0700

    feat (routes/web): update home route to use HomeController

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GuestBookController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResidentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('home'))->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function () {
     Route::get('/', DashboardController::class)->name('dashboard');
