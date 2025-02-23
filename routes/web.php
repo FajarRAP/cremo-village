31f7b884fbccf16b38cd66b429142e430839bd5e
FajarRiansyah <fajary781@gmail.com>
2025-02-23 18:44:04 +0700

    chore (routes/web): change delete to destroy

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function ()
         Route::get('/resident-records/{resident}/edit', 'edit')->name('resident.edit');
         Route::post('/resident-records', 'store')->name('resident.store');
         Route::put('/resident-records/{resident}', 'update')->name('resident.update');
-        Route::delete('/resident-records/{resident}', 'delete')->name('resident.delete');
+        Route::delete('/resident-records/{resident}', 'destroy')->name('resident.destroy');
     });
 
     Route::controller(GuestBookController::class)->group(function () {
         Route::get('/guest-book', 'index')->name('dashboard.guest-book');
         Route::get('/guest-book/{guestbook}/edit', 'edit')->name('guest-book.edit');
-        Route::post('/guest-book', 'store')->name('resident.store');
+        Route::post('/guest-book', 'store')->name('guest-book.store');
         Route::put('/guest-book/{guestbook}', 'update')->name('guest-book.update');
-        Route::delete('/guest-book/{guestbook}', 'delete')->name('guest-book.delete');
+        Route::delete('/guest-book/{guestbook}', 'destroy')->name('guest-book.destroy');
     });
 
     Route::controller(AgendaController::class)->group(function () {
@@ -33,7 +33,7 @@ Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function ()
         Route::get('/agenda/{agenda}/edit', 'edit')->name('agenda.edit');
         Route::post('/agenda', 'store')->name('agenda.store');
         Route::put('/agenda/{agenda}', 'update')->name('agenda.update');
-        Route::delete('/agenda/{agenda}', 'delete')->name('agenda.delete');
+        Route::delete('/agenda/{agenda}', 'destroy')->name('agenda.destroy');
     });
 
     Route::get('/news', fn() => view('dashboard.news'))->name('dashboard.news');
