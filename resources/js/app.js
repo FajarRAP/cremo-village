b71469818537142c0d96a0b04710196c71bf7181
FajarRiansyah <fajary781@gmail.com>
2025-02-24 00:06:34 +0700

    feat: add alpine js collapse plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,10 @@ import "../css/app.css";
 import.meta.glob(["../images/**", "../fonts/**"]);
 import Alpine from "alpinejs";
 import intersect from "@alpinejs/intersect";
-
+import collapse from "@alpinejs/collapse";
 
 window.Alpine = Alpine;
 
 Alpine.plugin(intersect);
+Alpine.plugin(collapse);
 Alpine.start();
