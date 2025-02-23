fcb98688b6b091b97b2f7b3a89f924e130bf003c
FajarRiansyah <fajary781@gmail.com>
2025-02-23 18:42:31 +0700

    feat (guest book views): implement search and modal and dropdown component

--- resources/views/dashboard/guest-book/edit.blade.php
+++ resources/views/dashboard/guest-book/edit.blade.php
@@ -3,13 +3,15 @@
         <div class="max-w-2xl px-4 py-8 mx-auto lg:py-16">
             <div class="flex justify-between items-center mb-4">
                 <h2 class="text-xl font-bold text-gray-900">{{ __('Edit') . ' ' . __('Guest Book') }}</h2>
-                <form action="{{ route('guest-book.delete', ['guestbook' => $guestbook]) }}" method="POST">
+                <form action="{{ route('guest-book.destroy', ['guestbook' => $guestbook]) }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <x-error-button>
-                        <x-svgs.trash type="submit" class="mr-2 fill-white" />
-                        {{ __('Delete') . ' ' . __('Data') }}
-                    </x-error-button>
+                    <a href="{{ route('guest-book.destroy', ['guestbook' => $guestbook]) }}">
+                        <x-error-button onclick="event.preventDefault(); this.closest('form').submit();">
+                            <x-svgs.trash type="submit" class="mr-2 fill-white" />
+                            {{ __('Delete') . ' ' . __('Data') }}
+                        </x-error-button>
+                    </a>
                 </form>
             </div>
             <form action="{{ route('guest-book.update', ['guestbook' => $guestbook]) }}" method="POST"
@@ -19,24 +21,29 @@
                 <div>
                     <x-input-label for="name" class="mb-2" :value="__('Name')" />
                     <x-text-input id="name" name="name" class="w-full" :value="$guestbook->name" />
-                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-1" />
                 </div>
                 <div>
                     <x-input-label for="origin" class="mb-2" :value="__('Origin')" />
                     <x-text-input id="origin" name="origin" class="w-full" :value="$guestbook->origin" />
+                    <x-input-error :messages="$errors->get('origin')" class="mt-1" />
                 </div>
                 <div>
                     <x-input-label for="visit_date" class="mb-2" value="Tanggal Kunjungan (YYYY-MM-DD)" />
-                    <x-text-input id="visit_date" name="visit_date" class="w-full" :value="$guestbook->visit_date" />
+                    <x-text-input id="visit_date" name="visit_date" class="w-full" type="datetime-local"
+                        :value="$guestbook->visit_date" />
+                    <x-input-error :messages="$errors->get('visit_date')" class="mt-1" />
                 </div>
                 <div>
                     <x-input-label for="purpose" class="mb-2" :value="__('Purpose')" />
                     <x-text-input id="purpose" name="purpose" class="w-full" :value="$guestbook->purpose" />
+                    <x-input-error :messages="$errors->get('purpose')" class="mt-1" />
                 </div>
                 <div>
                     <x-input-label for="description" class="mb-2" :value="__('Description')" />
                     <x-x-text-area-input id="description" name="description" class="w-full" rows="5"
                         :value="$guestbook->description" />
+                    <x-input-error :messages="$errors->get('description')" class="mt-1" />
                 </div>
                 <div class="flex items-center space-x-4">
                     <x-primary-button type="submit">
--- resources/views/dashboard/guest-book/index.blade.php
+++ resources/views/dashboard/guest-book/index.blade.php
@@ -5,7 +5,7 @@
                 <div
                     class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                     <div class="w-full md:w-1/2">
-                        <form action="{{ route('dashboard.resident-records') }}" method="GET" class="flex items-center">
+                        <form action="{{ route('dashboard.guest-book') }}" method="GET" class="flex items-center">
                             <div class="relative w-full">
                                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <x-svgs.magnifying-glass class="fill-gray-500" />
@@ -17,9 +17,9 @@
                     </div>
                     <div
                         class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <button type="button" id="createProductModalButton" data-modal-target="createProductModal"
-                            data-modal-toggle="createProductModal"
-                            class="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2">
+                        <button type="button"
+                            class="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2"
+                            x-on:click="$dispatch('open-modal', 'add-guest-book')">
                             <x-svgs.plus class="w-3.5 mr-2 fill-white" />
                             {{ __('Add') . ' ' . __('Guest Book') }}
                         </button>
@@ -50,32 +50,35 @@
                                     <td class="px-4 py-3">{{ $guestbook->purpose }}</td>
                                     <td class="px-4 py-3">{{ $guestbook->description }}</td>
                                     <td class="px-4 py-3">
-                                        <button data-dropdown-toggle="{{ $loop->index }}"
-                                            class="inline-flex items-center text-sm font-medium px-1 rounded transition duration-200 hover:bg-gray-100"
-                                            type="button">
-                                            <x-svgs.ellipsis />
-                                        </button>
-                                        <div id="{{ $loop->index }}"
-                                            class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow">
-                                            <ul class="py-1 text-sm">
-                                                <li>
-                                                    <a href="{{ route('guest-book.edit', ['guestbook' => $guestbook]) }}"
-                                                        class="flex w-full items-center py-2 px-4 hover:bg-gray-100">
-                                                        <x-svgs.pen-to-square class="mr-2 fill-gray-500" />
-                                                        {{ __('Edit') }}
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <button type="button" data-modal-target="readProductModal"
-                                                        data-modal-toggle="readProductModal"
-                                                        class="flex w-full items-center py-2 px-4 hover:bg-gray-100"
-                                                        x-on:click="guestbook = {{ json_encode($guestbook) }}">
-                                                        <x-svgs.eye class="mr-2 fill-gray-500" />
-                                                        {{ __('Preview') }}
-                                                    </button>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                        <x-dropdown>
+                                            <x-slot name="trigger">
+                                                <button
+                                                    class="inline-flex items-center text-sm font-medium px-1 rounded transition duration-200 hover:bg-gray-100"
+                                                    type="button">
+                                                    <x-svgs.ellipsis />
+                                                </button>
+                                            </x-slot>
+                                            <x-slot name="content">
+                                                <ul class="py-1 text-sm">
+                                                    <li>
+                                                        <x-dropdown-link class="!flex items-center !text-gray-500"
+                                                            :href="route('guest-book.edit', [
+                                                                'guestbook' => $guestbook,
+                                                            ])">
+                                                            <x-svgs.pen-to-square class="mr-2 fill-gray-500" />
+                                                            {{ __('Edit') }}
+                                                        </x-dropdown-link>
+                                                    </li>
+                                                    <li class="hover:cursor-pointer">
+                                                        <x-dropdown-link class="!flex items-center !text-gray-500"
+                                                            x-on:click="guestbook = {{ json_encode($guestbook) }}; $dispatch('open-modal', 'preview-guest-book')">
+                                                            <x-svgs.eye class="mr-2 fill-gray-500" />
+                                                            {{ __('Preview') }}
+                                                        </x-dropdown-link>
+                                                    </li>
+                                                </ul>
+                                            </x-slot>
+                                        </x-dropdown>
                                     </td>
                                 </tr>
                             @endforeach
@@ -87,88 +90,97 @@
             </div>
         </div>
 
-        <div id="createProductModal"
-            class="hidden overflow-y-auto bg-gray-900/40 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full">
-            <div class="relative p-4 w-full max-w-2xl max-h-full">
-                <div class="relative p-4 bg-white rounded-lg shadow sm:p-5">
+        <x-modal name="add-guest-book" :show="$errors->storeGuestBook->isNotEmpty()" focusable>
+            <form action="{{ route('guest-book.store') }}" method="POST" class="text-left">
+                @csrf
+                @method('POST')
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 space-y-4">
                     <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
                         <h3 class="text-lg font-semibold text-gray-900 Product">
                             {{ __('Add') . ' ' . __('Guest Book') }}</h3>
                         <button type="button"
-                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                            data-modal-target="createProductModal" data-modal-toggle="createProductModal">
-                            <x-svgs.xmark />
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                            <x-svgs.xmark x-on:click="$dispatch('close-modal', 'add-guest-book')" />
                         </button>
                     </div>
-                    <form action="{{ route('resident.store') }}" method="POST">
-                        @csrf
-                        <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                            <div>
-                                <x-input-label for="name" class="mb-2" :value="__('Name')" />
-                                <x-text-input id="name" name="name" class="w-full" />
-                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                            </div>
-                            <div>
-                                <x-input-label for="origin" class="mb-2" :value="__('Origin')" />
-                                <x-text-input id="origin" name="origin" class="w-full" />
-                            </div>
-                            <div>
-                                <x-input-label for="visit_date" class="mb-2" value="Tanggal Kunjungan (YYYY-MM-DD)" />
-                                <x-text-input id="visit_date" name="visit_date" class="w-full" />
-                            </div>
-                            <div>
-                                <x-input-label for="purpose" class="mb-2" :value="__('Purpose')" />
-                                <x-text-input id="purpose" name="purpose" class="w-full" />
-                            </div>
-                            <div class="md:col-span-2">
-                                <x-input-label for="description" class="mb-2" :value="__('Description')" />
-                                <x-x-text-area-input id="description" name="description" class="w-full"
-                                    rows="5" />
-                            </div>
-                        </div>
-                        <button type="submit"
-                            class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-                            <x-svgs.plus class="mr-2 fill-white" />
-                            {{ __('Add') }}
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <div id="readProductModal"
-            class="hidden overflow-y-auto bg-gray-900/40 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full">
-            <div class="relative p-4 w-full max-w-xl max-h-full">
-                <div class="relative p-4 bg-white rounded-lg shadow sm:p-5">
-                    <div class="flex justify-between mb-4 rounded-t sm:mb-5">
-                        <div class="text-lg text-gray-900 md:text-xl">
-                            <h3 class="font-semibold">Nomor NIK</h3>
-                            <p class="text-base md:text-lg font-bold">Nomor KK</p>
+                    <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                        <div>
+                            <x-input-label for="name" class="mb-2" :value="__('Name')" />
+                            <x-text-input id="name" name="name" class="w-full" />
+                            <x-input-error :messages="$errors->storeGuestBook->get('name')" class="mt-1" />
                         </div>
                         <div>
-                            <button type="button"
-                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex"
-                                data-modal-toggle="readProductModal">
-                                <x-svgs.xmark class="fill-gray-400" />
-                            </button>
+                            <x-input-label for="origin" class="mb-2" :value="__('Origin')" />
+                            <x-text-input id="origin" name="origin" class="w-full" />
+                            <x-input-error :messages="$errors->storeGuestBook->get('origin')" class="mt-1" />
+                        </div>
+                        <div>
+                            <x-input-label for="visit_date" class="mb-2" :value="__('Tanggal Kunjungan (YYYY-MM-DD)')" />
+                            <x-text-input id="visit_date" name="visit_date" class="w-full" type="datetime-local" />
+                            <x-input-error :messages="$errors->storeGuestBook->get('visit_date')" class="mt-1" />
+                        </div>
+                        <div>
+                            <x-input-label for="purpose" class="mb-2" :value="__('Purpose')" />
+                            <x-text-input id="purpose" name="purpose" class="w-full" />
+                            <x-input-error :messages="$errors->storeGuestBook->get('purpose')" class="mt-1" />
+                        </div>
+                        <div class="md:col-span-2">
+                            <x-input-label for="description" class="mb-2" :value="__('Description')" />
+                            <x-x-text-area-input id="description" name="description" class="w-full" rows="5" />
+                            <x-input-error :messages="$errors->storeGuestBook->get('description')" class="mt-1" />
                         </div>
                     </div>
-                    <dl>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900">{{ __('Name') }}</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5" x-text="guestbook.name"></dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900">{{ __('Origin') }}</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5" x-text="guestbook.origin"></dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900">{{ __('Date') }}</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5"
-                            x-text="new Intl.DateTimeFormat('id-ID', {day: '2-digit', month: '2-digit', year: 'numeric'}).format(new Date(guestbook.visit_date))">
-                        </dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900">{{ __('Purpose') }}</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5" x-text="guestbook.purpose"></dd>
-                        <dt class="mb-2 font-semibold leading-none text-gray-900">{{ __('Description') }}</dt>
-                        <dd class="mb-4 font-light text-gray-500 sm:mb-5" x-text="guestbook.description"></dd>
-                    </dl>
                 </div>
+                <div class="bg-gray-50 px-4 py-3 sm:flex sm:px-6">
+                    <button type="submit"
+                        class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                        <x-svgs.plus class="mr-2 fill-white" />
+                        {{ __('Add') }}
+                    </button>
+                </div>
+            </form>
+        </x-modal>
+
+        <x-modal name="preview-guest-book" focusable>
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 space-y-4 text-left">
+                <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5">
+                    <h3 class="text-lg font-semibold text-gray-900">
+                        {{ __('Preview') . ' ' . __('Guest Book') }}
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+                        x-on:click="$dispatch('close-modal', 'preview-guest-book')">
+                        <x-svgs.xmark />
+                    </button>
+                </div>
+                <p class="text-gray-700">
+                    <strong>{{ __('Name') }}:</strong>
+                    <span x-text="guestbook.name"></span>
+                </p>
+                <p class="text-gray-700">
+                    <strong>{{ __('Origin') }}:</strong>
+                    <span x-text="guestbook.origin"></span>
+                </p>
+                <p class="text-gray-700">
+                    <strong>{{ __('Visit Date') }}:</strong>
+                    <span x-text="guestbook.visit_date"></span>
+                </p>
+                <p class="text-gray-700">
+                    <strong>{{ __('Purpose') }}:</strong>
+                    <span x-text="guestbook.purpose"></span>
+                </p>
+                <p class="text-gray-700">
+                    <strong>{{ __('Description') }}:</strong>
+                    <span x-text="guestbook.description"></span>
+                </p>
             </div>
-        </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:px-6">
+                <button type="button"
+                    class="text-gray-700 inline-flex items-center bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition"
+                    x-on:click="$dispatch('close-modal', 'preview-guest-book')">
+                    {{ __('Close') }}
+                </button>
+            </div>
+        </x-modal>
     </section>
 </x-dashboard-layout>
