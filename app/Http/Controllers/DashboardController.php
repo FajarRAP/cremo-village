6039f0031d936038b8f95fd08dffd7f2dedcc686
FajarRiansyah <fajary781@gmail.com>
2025-02-23 13:07:55 +0700

    feat: add dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,28 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Agenda;
 use App\Models\GuestBook;
 use App\Models\Resident;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $guestbook = new GuestBook();
         $resident = new Resident();
+
         $residentCount = $resident->count();
         $residents = $resident->latest()->limit(5)->get();
         $guestbookCount = $guestbook->count();
         $guestbooks = $guestbook->latest()->limit(5)->get();
+        $agendas = $this->getAgendas($request->input('date'));
+
 
         return view('dashboard.dashboard', [
             'residentCount' => $residentCount,
             'guestbookCount' => $guestbookCount,
             'guestbooks' => $guestbooks,
             'residents' => $residents,
+            'agendas' => $agendas,
         ]);
+    }
+
+    private function getAgendas(?string $date)
+    {
+        $agendas = Agenda::orderByDesc('date');
+
+        if ($date) {
+            [$year, $month] = explode('-', $date);
+            $agendas->whereYear('date', '=', $year)
+                ->whereMonth('date', '=', $month);
+        } else {
+            $agendas->limit(5);
+        }
+
+        return $agendas->get();
     }
 }
